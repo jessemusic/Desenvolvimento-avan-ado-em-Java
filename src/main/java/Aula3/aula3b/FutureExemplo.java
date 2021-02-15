721fe95cb8eeb52def351a7efd1a5ee6eded8d17
jessemusic <jmc_cursos@hotmail.com>
2021-02-15 00:51:49 -0300

    Future no java Atividades da casa resolvidas

--- src/main/java/Aula3/aula3b/FutureExemplo.java
+++ src/main/java/Aula3/aula3b/FutureExemplo.java
@@ -3,23 +3,54 @@ package Aula3.aula3b;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class FutureExemplo {
-    private static final ExecutorService threadPool = Executors.newFixedThreadPool( 3 );
+    private static final ExecutorService pessoasParaExecutarAtividades = Executors.newFixedThreadPool( 3 );
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Casa casa = new Casa( new Quarto() );
-        casa.obterAfazeresDaCasa().forEach( atividade -> threadPool.execute( () ->
-        {
-            try {
-                atividade.realizar();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        List<? extends Future<String>> futuros =
+        new CopyOnWriteArrayList<>(casa.obterAfazeresDaCasa().stream()
+                .map(atividade -> pessoasParaExecutarAtividades.submit(() -> {
+                    try {
+                        return atividade.realizar();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                )
+                .collect( Collectors.toList()));
+
+        while (true){
+            int numeroDeAtividadesNaoFinalizadas = 0;
+            for(Future<?> futuro  : futuros ) {
+                if (futuro.isDone()){
+                    try {
+                        System.out.println("Parabéns você terminou de " + futuro.get());
+                        futuros.remove( futuro );
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    numeroDeAtividadesNaoFinalizadas++;
+                }
             }
-        } ) );
-        threadPool.shutdown();
+            if(futuros.stream().allMatch(Future::isDone)){
+                break;
+            }
+            System.out.println("Números de atividades não finalizadas : " +numeroDeAtividadesNaoFinalizadas );
+            Thread.sleep( 500 );
+        }
+
+
+        pessoasParaExecutarAtividades.shutdown();
     }
 
 
@@ -38,7 +69,7 @@ class Casa {
 }
 
 interface  Atividade{
-    void realizar() throws InterruptedException;
+    String realizar() throws InterruptedException;
 }
 abstract  class Comodo {
     abstract List<Atividade> obterAfazeresDoComodo();
@@ -53,14 +84,23 @@ class Quarto extends Comodo{
                 this::arrumaOGuardaRoupa
         );
     }
-    private void arrumaOGuardaRoupa() throws InterruptedException {
+    private String arrumaOGuardaRoupa() throws InterruptedException {
         Thread.sleep( 5000 );
-        System.out.println("arruma o guarda roupa"); }
-    private void varreOQuarto() throws InterruptedException {
-        Thread.sleep( 5000 );
-        System.out.println("varre o quarto");}
-    private void arrumaACama() throws InterruptedException {
-        Thread.sleep( 5000 );
-        System.out.println("Arruma a cama");}
+        String arruma_o_guarda_roupa = "arruma o guarda roupa";
+        System.out.println("arruma o guarda roupa");
+        return arruma_o_guarda_roupa;
+    }
+    private String varreOQuarto() throws InterruptedException {
+        Thread.sleep( 7000 );
+        String varre_o_quarto = "varre o quarto";
+        System.out.println("varre o quarto");
+        return varre_o_quarto;
+    }
+    private String arrumaACama() throws InterruptedException {
+        Thread.sleep( 9000 );
+        String arruma_a_cama ="Arruma a cama";
+        System.out.println("Arruma a cama");
+        return arruma_a_cama;
+    }
 
 }
